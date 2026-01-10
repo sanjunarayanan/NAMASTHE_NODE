6865072765b416203ced32d01f740dbc51292bb0
Sanju Narayanan <sanjunarayanan123@gmail.com>
2026-01-10 23:29:12 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -2,17 +2,32 @@ import express from 'express'
 import connectDb from './config/database.js'
 import User from './models/user.model.js'
 const app = express()
-
+app.use(express.json())
 await connectDb()
 
-app.post('/signup', async (req,res) => {
-  const sanju = new User({
-    firstName: 'sain',
-    lastName: 'tegular',
-    address: 'India House',
-  })
-  await sanju.save()
-  res.send('user successfully added')
+app.post('/user', async (req, res) => {
+  try {
+    const user = new User(req.body)
+    if (user) {
+      await user.save()
+      res.send('user added successful')
+    }
+    res.status(404).send('user is not found..!')
+  } catch (error) {
+    res.status(400).send('something went wrong..!')
+  }
+})
+
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({})
+    if (users.length) {
+      res.send(users)
+    }
+    res.status(404).send('user is not found..!')
+  } catch (error) {
+    res.status(400).send('something went wrong..!')
+  }
 })
 
 app.listen(3000, () => {
